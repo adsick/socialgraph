ea0d88e3ead74b39c05afae11095284f0ab029c7
adsick <adsick@protonmail.com>
2021-12-18 10:23:12 +0200

    beta

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::BTreeMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::UnorderedMap;
 use near_sdk::{near_bindgen, AccountId, env};
@@ -8,50 +8,35 @@ use near_sdk::{near_bindgen, AccountId, env};
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
-    relations: UnorderedMap<AccountId, HashSet<RelationKind>>,
+    connections: UnorderedMap<AccountId, BTreeMap<AccountId, (u8, u8)>>,
+    // connections_rev: UnorderedMap<>,
+    // familiarity: UnorderedMap<AccountId, HashSet<(AccountId, u8)>>, //knowledge map
+    // dependancies: UnorderedMap<AccountId, HashSet<(AccountId, u8)>>,
+    
+    // know: UnorderedMap<AccountId, HashSet<(AccountId, u8)>>,
+    // known_by: UnorderedSet<AccountId>,
 }
 
 impl Default for Contract {
     fn default() -> Self {
         Self {
-            relations: UnorderedMap::new(b"r"),
+            connections: UnorderedMap::new(b"c")
         }
     }
-}
-
-// note, you can use u8 bitmap to express 8 kinds of relations
-// but we stick to enum for now
-
-#[derive(BorshDeserialize, BorshSerialize, PartialEq, Eq, Hash, PartialOrd)]
-pub enum RelationKind{
-    Knows(AccountId, u8),
-    DependsOn(AccountId, u8),
-    Follows(AccountId),
-    Loves(AccountId, u8)
-    // WorksFor(AccountId, u8)
-    // Partners(AccountId, u8)
 }
 
 #[near_bindgen]
 impl Contract {
     #[payable]
-    pub fn know(&mut self, mate: AccountId, distance: u8){
+    pub fn connect(&mut self, to: AccountId, distance: u8){
         let predecessor = env::predecessor_account_id();
-        self.add_relation(predecessor, RelationKind::Knows(mate, distance))
+        let mut connections = self.connections.get(&predecessor).unwrap_or_default();
+        
+        connections.insert(to, (1, distance));
+        self.connections.insert(&predecessor, &connections);
     }
 
-    pub fn depend_on(&mut self, account_id: AccountId, distance: u8){
-        let predecessor = env::predecessor_account_id();
-        self.add_relation(predecessor, RelationKind::DependsOn(account_id, distance))
-    }
-
-    fn add_relation(&mut self, account_id: AccountId, relation: RelationKind){
-        let mut relations = self.relations.get(&account_id).unwrap_or_default();
-        relations.insert(relation);
-        self.relations.insert(&account_id, &relations);
-    }
-
-    pub fn get_relations(&self, account_id: AccountId)->HashSet<RelationKind>{
-        self.relations.get(&account_id).expect("no relations")
+    pub fn get_connections_for(&self, account_id: AccountId)->BTreeMap<AccountId, (u8, u8)>{
+        self.connections.get(&account_id).expect("not found")
     }
 }
